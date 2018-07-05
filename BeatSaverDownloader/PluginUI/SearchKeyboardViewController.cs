39970f81eb113c90a64640e6917197fa0a818364
andruzzzhka <andruzzzhka@yandex.ru>
2018-07-05 18:49:08 +0700

    Added back button and numbers to the search keyboard

--- BeatSaverDownloader/PluginUI/SearchKeyboardViewController.cs
+++ BeatSaverDownloader/PluginUI/SearchKeyboardViewController.cs
@@ -11,9 +11,12 @@ namespace BeatSaverDownloader.PluginUI
 {
     class SearchKeyboardViewController : VRUIViewController
     {
-        UIKeyboard _searchKeyboard;
+        GameObject _searchKeyboardGO;
+
+        CustomUIKeyboard _searchKeyboard;
 
         Button _searchButton;
+        Button _backButton;
 
         TextMeshProUGUI _inputText;
         public string _inputString = "";
@@ -24,14 +27,17 @@ namespace BeatSaverDownloader.PluginUI
         {
             if (_searchKeyboard == null)
             {
-                _searchKeyboard = Instantiate(Resources.FindObjectsOfTypeAll<UIKeyboard>().First(), rectTransform, false);
+                _searchKeyboardGO = Instantiate(Resources.FindObjectsOfTypeAll<UIKeyboard>().First(), rectTransform, false).gameObject;
+
+                _searchKeyboard = _searchKeyboardGO.AddComponent<CustomUIKeyboard>();
+
                 _searchKeyboard.uiKeyboardKeyEvent = delegate (char input) { _inputString += input; UpdateInputText(); };
                 _searchKeyboard.uiKeyboardDeleteEvent = delegate () { _inputString = _inputString.Substring(0, _inputString.Length - 1); UpdateInputText(); };
             }
 
             if(_inputText == null)
             {
-                _inputText = BeatSaberUI.CreateText(rectTransform,"Search...", new Vector2(0f,-17.5f));
+                _inputText = BeatSaberUI.CreateText(rectTransform,"Search...", new Vector2(0f,-11.5f));
                 _inputText.alignment = TextAlignmentOptions.Center;
                 _inputText.fontSize = 6f;
             }
@@ -46,7 +52,7 @@ namespace BeatSaverDownloader.PluginUI
                 _searchButton = BeatSaberUI.CreateUIButton(rectTransform, "ApplyButton");
                 BeatSaberUI.SetButtonText(ref _searchButton, "Search");
                 (_searchButton.transform as RectTransform).sizeDelta = new Vector2(30f, 10f);
-                (_searchButton.transform as RectTransform).anchoredPosition = new Vector2(-15f, 5f);
+                (_searchButton.transform as RectTransform).anchoredPosition = new Vector2(-15f, 1.5f);
                 _searchButton.onClick.RemoveAllListeners();
                 _searchButton.onClick.AddListener(delegate() {
                     searchButtonPressed?.Invoke(_inputString);
@@ -54,8 +60,17 @@ namespace BeatSaverDownloader.PluginUI
                 });
             }
 
+            if (_backButton == null)
+            {
+                _backButton = BeatSaberUI.CreateBackButton(rectTransform);
 
-            base.DidActivate(firstActivation, activationType);
+                _backButton.onClick.AddListener(delegate ()
+                {
+                    _inputString = "";
+                    DismissModalViewController(null, false);
+                });
+            }
+
         }
 
         void UpdateInputText()
